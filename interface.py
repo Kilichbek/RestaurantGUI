7bc0d776744585ff0530f7c643d6e48c9c0e0653
Kilichbek Haydarov <kilich95@gmail.com>
2018-06-12 06:17:17 +0900

    Comment all classes and their methods

--- interface.py
+++ interface.py
@@ -1,4 +1,4 @@
-""""
+"""
 File: interface.py
 Authors:
         Name                        Student ID        Major                                     Role
@@ -17,6 +17,7 @@ Version: 1.0.0
 
 """
 
+
 import tkinter as tk
 import tkinter.messagebox as tm
 from tkinter import ttk
@@ -31,8 +32,18 @@ LARGE_FONT = ("Verdana", 12)
 logged_in = False
 
 class Root(tk.Tk):
+    """
+    Class to create the main frame for holding
+    all secondary frames
 
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Construct main frame by extending parent class tk.Tk
+
+        :param args: a non-keyworded, variable-length argument list
+        :param kwargs: a keyworded, variable-length argument list
+        """
         tk.Tk.__init__(self, *args, **kwargs)
         tk.Tk.iconbitmap(self,default="logo_icon.ico")
         tk.Tk.wm_title(self,"Restaurant Management System")
@@ -42,34 +53,52 @@ class Root(tk.Tk):
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0,weight=1)
 
+        # Create page objects
         self.frames = {}
-
         for F in (StartPage, PageOne, PageTwo,LoginPage,Today,):
 
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
+        # If a user successully logged in, Start Page is displayed, otherwise again Login Page
         if logged_in:
             self.show_frame(StartPage)
         else:
-            self.show_frame(StartPage)
+            self.show_frame(LoginPage)
 
     def show_frame(self,cont):
+        """
+        Display the frame based on the passed argument
+
+        :param cont: container for page frame to be displayed
+        :return: None
+        """
         frame = self.frames[cont]
         frame.tkraise()
 
 class StartPage(tk.Frame):
+    """
+    Class for Initial Page which contains
+    few buttons to navigate between other pages
+
+    """
     def __init__(self,parent,controller):
+        """
+        Construct a page by extending built-in class tk.Frame
+
+        :param parent: a container which can be obtained from Root Class
+        :param controller: the main frame
+        """
+
         tk.Frame.__init__(self,parent)
         label = tk.Label(self,text="Start Page",font=LARGE_FONT)
         label.pack(pady = 10,padx = 10)
 
 
-        button1 = ttk.Button(self,text="Visit Page 1",
+        button1 = ttk.Button(self,text="Page One",
                             command=lambda:controller.show_frame(PageOne))
-
-        button2 = ttk.Button(self, text="Visit Page 2",
+        button2 = ttk.Button(self, text="Tabels",
                             command=lambda: controller.show_frame(PageTwo))
         button3 = ttk.Button(self, text="Today",
                              command=lambda: controller.show_frame(Today))
@@ -117,7 +146,19 @@ class StartPage(tk.Frame):
 
 """
 class Today(tk.Frame):
+    """
+    Class for Today Page which displays today's income in a graph
+    using matplotlib
+
+    """
     def __init__(self, parent, controller):
+        """
+        Construct a page by extending built-in class tk.Frame
+
+        :param parent: a container which can be obtained from Root Class
+        :param controller: the main frame
+        """
+
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Today Graph", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
@@ -125,10 +166,12 @@ class Today(tk.Frame):
 
         button1 = ttk.Button(self, text="Home",
                              command=lambda: controller.show_frame(StartPage))
-        button1.pack()
         button2 = ttk.Button(self, text="Income Graph",
                              command=lambda: controller.show_frame(PageOne))
+
+        button1.pack()
         button2.pack()
+
         # Connection to Database and querying data from connected db
         con = lite.connect('incomes.db')
         cursor = con.execute("SELECT hours,hourly_income from today")
@@ -155,8 +198,18 @@ class Today(tk.Frame):
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 class PageOne(tk.Frame):
+    """
+        Class to create a dummy page which can be used
+        for some purpose later
 
+    """
     def __init__(self,parent,controller):
+        """
+            Construct a page by extending built-in class tk.Frame
+
+            :param parent: a container which can be obtained from Root Class
+            :param controller: the main frame
+        """
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Page one", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
@@ -164,41 +217,65 @@ class PageOne(tk.Frame):
         button1 = ttk.Button(self, text="Back to Home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
-        button2 = ttk.Button(self, text="Page Two",
+        button2 = ttk.Button(self, text="Tabels",
                            command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
 class PageTwo(tk.Frame):
+    """
+        Class to create a second page for keeping track of tabels
+        in the restaurant and menu options
 
+    """
     def __init__(self,parent,controller):
+        """
+            Construct a page by extending built-in class tk.Frame
+
+            :param parent: a container which can be obtained from Root Class
+            :param controller: the main frame
+        """
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Three", font=LARGE_FONT)
+        label = tk.Label(self, text="Tabels", font=LARGE_FONT)
         self.grid()
 
         for r in range(6):
             self.master.rowconfigure(r, weight=1)
 
-        Frame1 = tk.Frame(self, bg="red")
+        Frame1 = tk.Frame(self)
         Frame1.grid(row=0, column=0, rowspan=3, columnspan=2, sticky="WENS")
-        Frame2 = tk.Frame(self, bg="green")
+        Frame2 = tk.Frame(self)
         Frame2.grid(row=0, column=2, rowspan=6, columnspan=3, sticky="WENS")
 
         self.listbox = tk.Listbox(Frame1)
-        self.listbox.insert(1, "Table 1")
-        self.listbox.insert(2, "Table 2")
-        self.listbox.insert(3, "Table 3")
-        self.listbox.insert(4, "Table 4")
-        self.listbox.insert(5, "Table 5")
-        self.listbox.insert(6, "Table 6")
+        for i in range(10):
+            self.listbox.insert(i+1, "Table {:d}".format(i+1))
         self.listbox.grid(row=1, column=0, rowspan=4, columnspan=3, sticky="WENS")
         self.listbox.focus()
-        self.listbox.bind('<Double-1>', lambda event, frame=Frame1: self.menuPopUpWindow(event, frame))
+        self.listbox.bind('<Double-1>', lambda event: self.menuPopUpWindow(event))
         self.listbox.bind('<<ListboxSelect>>', lambda event,frame=Frame2:self.onselect(event,frame))
 
         menu_button = ttk.Button(Frame1,text="Close Table",command = lambda: self.closeTable())
         menu_button.grid(row=5,column = 0,columnspan = 3,sticky="WENS")
 
+        button1 = ttk.Button(self, text="Page One",
+                             command=lambda: controller.show_frame(PageOne))
+        button2 = ttk.Button(self, text="Tabels",
+                             command=lambda: controller.show_frame(PageTwo))
+        button3 = ttk.Button(self, text="Today",
+                             command=lambda: controller.show_frame(Today))
+
+        button1.grid(row=6,column=0,sticky="WENS")
+        button2.grid(row=6,column=1,sticky="WENS")
+        button3.grid(row=6,column=2,sticky="WENS")
+
     def closeTable(self):
+        """
+        Calculates total price of the selected tabel and sends to the database
+        and the cleans the content of the file associated with that table for
+        later purpose
+
+        :return: None
+        """
         table = self.selection_get() #TODO Adding total price and date to database
         file = open("Files/"+table+".txt","r")
         total_price = 0.0
@@ -210,6 +287,13 @@ class PageTwo(tk.Frame):
         self.treeview.delete(*self.treeview.get_children())
 
     def onselect(self,event,frame):
+        """
+        Prints the all ordered meals for the selected tabel and total price as well
+
+        :param event: SELECT event passed by the .bind(<event>,<callback function> method)
+        :param frame: on this frame the list of ordered meals is displayed
+        :return: None
+        """
         self.treeview = ttk.Treeview(frame)
         self.treeview.grid(row=0, column=2, rowspan=6, columnspan=3, sticky="WENS")
         self.treeview.config(columns=('name', 'price','amount','total'))
@@ -231,7 +315,15 @@ class PageTwo(tk.Frame):
 
         file.close()
 
-    def menuPopUpWindow(self,event,frame):
+    def menuPopUpWindow(self,event):
+        """
+        Pop-up window which contains the list of meals and drinks
+         is displayed when a user double-clicks on any tabel
+         All meals are retrieved from the menu.txt file
+
+        :param event: DOUBLE_CLICK event passed by the .bind(<event>,<callback function> method)
+        :return: None
+        """
         win = tk.Toplevel()
         win.wm_title("Menu")
         win.geometry("500x500+30+30")
@@ -255,7 +347,6 @@ class PageTwo(tk.Frame):
             self.treeviews[-1].heading('#0', text='#')
             self.treeviews[-1].heading('name', text='Name')
             self.treeviews[-1].heading('price', text='Price')
-            #self.treeviews[-1].insert('', 'end', text="1", values=("French Fries", "$3"))
             self.treeviews[-1].bind('<Double-1>', lambda x,num = i: self.amountPopUpWindow(event, table=tabel_no,
                                                                                               index=num))
             notebook_frames[-1].rowconfigure(0, weight=1)
@@ -284,6 +375,16 @@ class PageTwo(tk.Frame):
         b.grid(row=5, column=0)
 
     def amountPopUpWindow(self,event,table,index):
+        """
+        Pop-up window is displayed when a user double-clicks on a meal from the list and
+        requires the user to enter the amount of food or drink to be ordered
+
+        :param event: DOUBLE_CLICK event passed by the .bind(<event>,<callback function> method)
+        :param table: The number of tabel which has ordered food
+        :param index: The number of frame which indicates the category of food
+        :return: None
+
+        """
         win = tk.Toplevel()
         win.wm_title(str(index)+"Amount")
         win.geometry("320x200+30+30")
@@ -303,6 +404,15 @@ class PageTwo(tk.Frame):
         b.grid(row=2, column=0)
 
     def _calculate(self,popup_window,tab,food,price):
+        """
+        Calculates total price for ordered meal based on its own price
+
+        :param popup_window: pop-up window which will be destroyed after the calucation is done
+        :param tab: the tabel number which is used to create a unique file associated with it
+        :param food: the name of food to be ordered
+        :param price: the price of food to be ordered
+        :return: None
+        """
 
         amount = int(self.entry_amount.get())
         file = open("Files/" + tab + ".txt", "a")
@@ -312,7 +422,16 @@ class PageTwo(tk.Frame):
 
 
 class LoginPage(tk.Frame):
+    """
+    Class for Login Page which is used for authentication
+    """
     def __init__(self,parent,controller):
+        """
+            Construct a page by extending built-in class tk.Frame
+
+            :param parent: a container which can be obtained from Root Class
+            :param controller: the main frame
+        """
         tk.Frame.__init__(self,parent)
 
         self.label_username = tk.Label(self,text="Username")
@@ -335,6 +454,14 @@ class LoginPage(tk.Frame):
         self.logbtn.grid(columnspan=2)
 
     def _login_btn_clicked(self,controller):
+        """
+        Validates entered usearname and password with True values
+        if the process is successful, it grants access to Start Page,
+        otherwise Login Error is showed
+
+        :param controller: the main frame
+        :return: None
+        """
         global logged_in
         username = self.entry_username.get()
         password = self.entry_password.get()
@@ -350,6 +477,6 @@ class LoginPage(tk.Frame):
 
 
 app = Root()
-#app.geometry("{0}x{1}+0+0".format(app.winfo_screenwidth(),
-                                 # app.winfo_screenheight()))
+app.geometry("{0}x{1}+0+0".format(app.winfo_screenwidth(),
+                                  app.winfo_screenheight()))
 app.mainloop()
